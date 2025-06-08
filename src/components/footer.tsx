f0877459729229e0d4602e8b66b7666d19e8af7b
Dima Neto <kuznetsovdv2013@gmail.com>
2025-06-08 14:23:38 +0300

    добавил header и footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../styles/footer.css';
+
+export const Footer: React.FC = () => {
+    return (
+        <div className="footer">
+            @ 2025 "Название сайта"
+        </div>
+    );
+};
---
+++ src/components/header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import '../styles/header.css';
+import logo from '../assets/logos/logo.png'
+
+export const Header: React.FC = () => {
+    return (
+        <header className="header">
+            <div className="header__logo">
+                <img src={logo} alt="Логотип" style={{ maxHeight: '50px' }}/>
+            </div>
+            <nav className="header__nav">
+                <a href="#biografy">Биография</a>
+                <a href="#portfolio">Портфолио</a>
+                <a href="#articles">Статьи</a>
+                <a href="#pricelist">Прайслист</a>
+                <a href="#contacts">Контакты</a>
+            </nav>
+            <h1 className="header__tittle">Ваш косметолог Регина</h1>
+        </header>
+    );
+};
